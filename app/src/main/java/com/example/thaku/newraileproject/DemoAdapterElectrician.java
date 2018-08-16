970cc92c5f0b10ec6b10ab1bfd42470db3d8da28
manisha2620 <manishathakuruploadpro@gmail.com>
2018-08-16 21:55:33 +0530

    first commit

--- app/src/main/java/com/example/thaku/newraileproject/DemoAdapterElectrician.java
+++ app/src/main/java/com/example/thaku/newraileproject/DemoAdapterElectrician.java
@@ -1,72 +1,72 @@
-package com.example.thaku.newraileproject;
-
-import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RatingBar;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-
-public class DemoAdapterElectrician extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-
-    ArrayList<ModelClass> data;
-    Context context;
-
-    public DemoAdapterElectrician(Context context,ArrayList<ModelClass>data){
-        this.context=context;
-        this.data=data;
-    }
-
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-DemoAdapterElectrician.MyViewHolder holder=new DemoAdapterElectrician.MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.multiple_item,parent,false));
-        return holder;    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        DemoAdapterElectrician.MyViewHolder myviewHolder=(DemoAdapterElectrician.MyViewHolder)holder;
-
-
-        myviewHolder.name.setText(data.get(position).getname());
-        myviewHolder.single_phone.setText(data.get(position).getSingle_phone());
-        myviewHolder.single_location.setText(data.get(position).getSingle_location());
-        myviewHolder.single_requirement.setText(data.get(position).getSingle_requirement());
-        myviewHolder.single_share.setText(data.get(position).getSingle_share());
-        myviewHolder.single_profile.setImageResource(data.get(position).getSingle_profile());
-        myviewHolder.single_electrical.setImageResource(data.get(position).getSingle_electrical());
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return data.size();
-    }
-
-
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        ImageView image_view, share, call, message;
-        RatingBar rating_bar;
-        TextView name, five, share_text, time, open, sector;
-
-        public MyViewHolder(View itemView) {
-            super(itemView);
-
-            image_view = itemView.findViewById(R.id.image_view);
-            share = itemView.findViewById(R.id.share);
-            call = itemView.findViewById(R.id.call);
-            message = itemView.findViewById(R.id.message);
-            rating_bar = itemView.findViewById(R.id.rating_bar);
-            five = itemView.findViewById(R.id.five);
-            share_text = itemView.findViewById(R.id.share_text);
-            open = itemView.findViewById(R.id.open);
-            sector = itemView.findViewById(R.id.sector);
-
-        }
-    }
-}
+//package com.example.thaku.newraileproject;
+//
+//import android.content.Context;
+//import android.support.annotation.NonNull;
+//import android.support.v7.widget.RecyclerView;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageView;
+//import android.widget.RatingBar;
+//import android.widget.TextView;
+//
+//import java.util.ArrayList;
+//
+//public class DemoAdapterElectrician extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+//
+//    ArrayList<ModelClass> data;
+//    Context context;
+//
+//    public DemoAdapterElectrician(Context context,ArrayList<ModelClass>data){
+//        this.context=context;
+//        this.data=data;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//DemoAdapterElectrician.MyViewHolder holder=new DemoAdapterElectrician.MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.multiple_item,parent,false));
+//        return holder;    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+//        DemoAdapterElectrician.MyViewHolder myviewHolder=(DemoAdapterElectrician.MyViewHolder)holder;
+//
+//
+//        myviewHolder.name.setText(data.get(position).getname());
+//        myviewHolder.single_phone.setText(data.get(position).getSingle_phone());
+//        myviewHolder.single_location.setText(data.get(position).getSingle_location());
+//        myviewHolder.single_requirement.setText(data.get(position).getSingle_requirement());
+//        myviewHolder.single_share.setText(data.get(position).getSingle_share());
+//        myviewHolder.single_profile.setImageResource(data.get(position).getSingle_profile());
+//        myviewHolder.single_electrical.setImageResource(data.get(position).getSingle_electrical());
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return data.size();
+//    }
+//
+//
+//    public class MyViewHolder extends RecyclerView.ViewHolder {
+//        ImageView image_view, share, call, message;
+//        RatingBar rating_bar;
+//        TextView name, five, share_text, time, open, sector;
+//
+//        public MyViewHolder(View itemView) {
+//            super(itemView);
+//
+//            image_view = itemView.findViewById(R.id.image_view);
+//            share = itemView.findViewById(R.id.share);
+//            call = itemView.findViewById(R.id.call);
+//            message = itemView.findViewById(R.id.message);
+//            rating_bar = itemView.findViewById(R.id.rating_bar);
+//            five = itemView.findViewById(R.id.five);
+//            share_text = itemView.findViewById(R.id.share_text);
+//            open = itemView.findViewById(R.id.open);
+//            sector = itemView.findViewById(R.id.sector);
+//
+//        }
+//    }
+//}
--- app/src/main/java/com/example/thaku/newraileproject/Electrician.java
+++ app/src/main/java/com/example/thaku/newraileproject/Electrician.java
@@ -1,74 +1,74 @@
-package com.example.thaku.newraileproject;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.WindowManager;
-
-import java.util.ArrayList;
-
-public class Electrician extends AppCompatActivity {
-    RecyclerView recyclerView;
-    //    Fragment fragment_recycler;
-    ArrayList<ModelClass> data=new ArrayList<>();
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_electrician);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        recyclerView=findViewById(R.id.recyclerView);
-
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-        ModelClass modelClass=new ModelClass();
-        ModelClass modelClass1=new ModelClass();
-        ModelClass modelClass2=new ModelClass();
-        ModelClass modelClass3=new ModelClass();
-        ModelClass modelClass4=new ModelClass();
-        ModelClass modelClass5=new ModelClass();
-        ModelClass modelClass6=new ModelClass();
-        ModelClass modelClass7=new ModelClass();
-        ModelClass modelClass8=new ModelClass();
-        ModelClass modelClass9=new ModelClass();
-        ModelClass modelClass10=new ModelClass();
-        ModelClass modelClass11=new ModelClass();
-
-        modelClass.setname("Kumar Electrician");
-        modelClass.setSingle_phone("Phone Number:- +91- 9999988888");
-        modelClass.setSingle_location("Location:- Sector 55Near Ab Colony Chandigarh(4Kms)");
-        modelClass.setSingle_requirement("Requirement of 2 electricians");
-        modelClass.setSingle_share("share");
-        modelClass.setSingle_profile(R.drawable.ic_user);
-        modelClass.setSingle_electrical(R.drawable.electrical);
-
-
-
-
-
-        data.add(modelClass);
-        data.add(modelClass1);
-        data.add(modelClass2);
-        data.add(modelClass3);
-        data.add(modelClass4);
-        data.add(modelClass5);
-        data.add(modelClass6);
-        data.add(modelClass7);
-        data.add(modelClass8);
-        data.add(modelClass9);
-        data.add(modelClass10);
-        data.add(modelClass11);
-
-
-        DemoAdapter demoAdapter=new DemoAdapter(Electrician.this,data);
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
-
-        recyclerView.setAdapter(demoAdapter);
-
-
-        recyclerView.setNestedScrollingEnabled(false);
-
-    }
-}
+//package com.example.thaku.newraileproject;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.support.v7.widget.DividerItemDecoration;
+//import android.support.v7.widget.LinearLayoutManager;
+//import android.support.v7.widget.RecyclerView;
+//import android.view.WindowManager;
+//
+//import java.util.ArrayList;
+//
+//public class Electrician extends AppCompatActivity {
+//    RecyclerView recyclerView;
+//    //    Fragment fragment_recycler;
+//    ArrayList<ModelClass> data=new ArrayList<>();
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_electrician);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        recyclerView=findViewById(R.id.recyclerView);
+//
+//        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getApplicationContext());
+//        recyclerView.setLayoutManager(linearLayoutManager);
+//
+//        ModelClass modelClass=new ModelClass();
+//        ModelClass modelClass1=new ModelClass();
+//        ModelClass modelClass2=new ModelClass();
+//        ModelClass modelClass3=new ModelClass();
+//        ModelClass modelClass4=new ModelClass();
+//        ModelClass modelClass5=new ModelClass();
+//        ModelClass modelClass6=new ModelClass();
+//        ModelClass modelClass7=new ModelClass();
+//        ModelClass modelClass8=new ModelClass();
+//        ModelClass modelClass9=new ModelClass();
+//        ModelClass modelClass10=new ModelClass();
+//        ModelClass modelClass11=new ModelClass();
+//
+//        modelClass.setname("Kumar Electrician");
+//        modelClass.setSingle_phone("Phone Number:- +91- 9999988888");
+//        modelClass.setSingle_location("Location:- Sector 55Near Ab Colony Chandigarh(4Kms)");
+//        modelClass.setSingle_requirement("Requirement of 2 electricians");
+//        modelClass.setSingle_share("share");
+//        modelClass.setSingle_profile(R.drawable.ic_user);
+//        modelClass.setSingle_electrical(R.drawable.electrical);
+//
+//
+//
+//
+//
+//        data.add(modelClass);
+//        data.add(modelClass1);
+//        data.add(modelClass2);
+//        data.add(modelClass3);
+//        data.add(modelClass4);
+//        data.add(modelClass5);
+//        data.add(modelClass6);
+//        data.add(modelClass7);
+//        data.add(modelClass8);
+//        data.add(modelClass9);
+//        data.add(modelClass10);
+//        data.add(modelClass11);
+//
+//
+//        DemoAdapter demoAdapter=new DemoAdapter(Electrician.this,data);
+//        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+//
+//        recyclerView.setAdapter(demoAdapter);
+//
+//
+//        recyclerView.setNestedScrollingEnabled(false);
+//
+//    }
+//}
